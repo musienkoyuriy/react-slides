09d7a893ca96462a74a09e78def62cc63e14507a
Yuriy <musienkoyuriy@gmail.com>
2015-08-29 00:24:13 +0300

    styles

--- src/sliderStyles.js
+++ src/sliderStyles.js
@@ -1,7 +1,20 @@
 let sliderCSS = {
 	'backgroundColor': '#f6f6f6',
 	'width': '700px',
+	'height': '450px',
+	'border': '15px solid grey'
+};
+
+let slideCSS = {
+	'width': '700px',
 	'height': '450px'
 };
 
-export default sliderCSS;
+let getStyles = () => {
+	return {
+		sliderCSS: sliderCSS,
+		slideCSS: slideCSS
+	};
+}
+
+export default getStyles;
