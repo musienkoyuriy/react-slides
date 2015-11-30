5a710bd34d0e2817f089f7bb8bcc71bcf44bf9dd
Yuriy Musienko <musienko@musienko.nixsolutions.com>
2015-11-30 16:23:04 +0200

    2 spaces instead

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -6,64 +6,64 @@ const ARROW_LEFT_KEY_CODE = 37;
 const ARROW_RIGHT_KEY_CODE = 39;
 
 export default class Slider extends Component {
-	constructor() {
-		super();
-		this.state = {
-			currentSlideIndex: 0
-		};
-	}	
+  constructor() {
+    super();
+    this.state = {
+      currentSlideIndex: 0
+    };
+  } 
 
-	propTypes: {
-		slides: React.PropTypes.array.isRequired,
-		interval: React.PropTypes.number
-	}
+  propTypes: {
+    slides: React.PropTypes.array.isRequired,
+    interval: React.PropTypes.number
+  }
 
-	_goBack() {
-		let currentSlideIndex = this.state.currentSlideIndex;
-		currentSlideIndex = currentSlideIndex !== 0 ? currentSlideIndex-1 : 0;
-		this.setState({currentSlideIndex: currentSlideIndex});
-	}
+  _goBack() {
+    let currentSlideIndex = this.state.currentSlideIndex;
+    currentSlideIndex = currentSlideIndex !== 0 ? currentSlideIndex-1 : 0;
+    this.setState({currentSlideIndex: currentSlideIndex});
+  }
 
-	_goForward() {
-		let currentSlideIndex = this.state.currentSlideIndex;
-		let lastIndex = this.props.slides.length - 1;
-		currentSlideIndex = currentSlideIndex !== lastIndex ? currentSlideIndex + 1 : lastIndex;
-		this.setState({currentSlideIndex: currentSlideIndex});
-	}
+  _goForward() {
+    let currentSlideIndex = this.state.currentSlideIndex;
+    let lastIndex = this.props.slides.length - 1;
+    currentSlideIndex = currentSlideIndex !== lastIndex ? currentSlideIndex + 1 : lastIndex;
+    this.setState({currentSlideIndex: currentSlideIndex});
+  }
 
-	_onKeyPressHandle(e) {
-		let pressedKey = e.keyCode;
-		if (pressedKey === ARROW_LEFT_KEY_CODE) {
-			this._goBack();
-			this.setTransitionInterval();
-		} else if (pressedKey === ARROW_RIGHT_KEY_CODE) {
-			this._goForward();
-			this.setTransitionInterval();
-		} else {
-			return false;
-		}
-	}
+  _onKeyPressHandle(e) {
+    let pressedKey = e.keyCode;
+    if (pressedKey === ARROW_LEFT_KEY_CODE) {
+      this._goBack();
+      this.setTransitionInterval();
+    } else if (pressedKey === ARROW_RIGHT_KEY_CODE) {
+      this._goForward();
+      this.setTransitionInterval();
+    } else {
+      return false;
+    }
+  }
 
-	componentDidMount() {
-		window.addEventListener('keydown', this._onKeyPressHandle.bind(this));
-		this.setTransitionInterval();
-	}
+  componentDidMount() {
+    window.addEventListener('keydown', this._onKeyPressHandle.bind(this));
+    this.setTransitionInterval();
+  }
 
-	setTransitionInterval() {
-		if (!this.props.interval) {
-			return false;
-		}
-		if (this.interval) {
-			clearInterval(this.interval);
-		}
-		this.interval = setInterval(this._goForward.bind(this), this.props.interval);
-	}
+  setTransitionInterval() {
+    if (!this.props.interval) {
+      return false;
+    }
+    if (this.interval) {
+      clearInterval(this.interval);
+    }
+    this.interval = setInterval(this._goForward.bind(this), this.props.interval);
+  }
 
-	render() {
-		return (
-			<div className="slider" style={getStyles().sliderCSS}>
-				 <Slide value={this.props.slides[this.state.currentSlideIndex]}/>;
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="slider" style={getStyles().sliderCSS}>
+         <Slide value={this.props.slides[this.state.currentSlideIndex]}/>;
+      </div>
+    );
+  }
 }
--- src/sliderStyles.js
+++ src/sliderStyles.js
@@ -1,13 +1,13 @@
 let sliderCSS = {
-	'backgroundColor': '#f6f6f6',
-	'width': '700px',
-	'height': '450px',
-	'border': '15px solid grey'
+  'backgroundColor': '#f6f6f6',
+  'width': '700px',
+  'height': '450px',
+  'border': '15px solid grey'
 };
 
 let slideCSS = { 'width': '700px', 'height': '450px' };
 
 export default () => {
-	return { sliderCSS, slideCSS };
+  return { sliderCSS, slideCSS };
 }
 
